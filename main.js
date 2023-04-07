b597092df4be3cb9bdf4a3c4d2aa1aa58e4c422b
SoyeonL <sylee932003@gmail.com>
2023-04-07 00:15:13 -0700

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Make navbar transparent when it is on the top
 const navbar = document.getElementById("navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -9,4 +10,17 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
